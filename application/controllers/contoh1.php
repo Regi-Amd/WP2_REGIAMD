6ffaca728155c13335153976bb2fb558803a8fd0
Regi-Amd <regi.amd99@gmail.com>
2021-09-25 22:36:15 +0700

    Menampilkan biodata contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+            echo '<h1>Perkenalan</h1>';
+            echo "nama saya Regi Amanda, saya tinggal di dramaga. hobi saya bermain futsal";
+    }
+}
