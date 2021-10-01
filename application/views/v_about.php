d124f4ee94e9a9ee6e993c629d301f60c9f40808
Regi-Amd <regi.amd99@gmail.com>
2021-10-01 23:22:58 +0700

    Menambahkan halaman about

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Regi</li>
+ <li>Nama Belakang : Amanda</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kp.Carang Pulang KUD 104</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bogor</li>
+ </ul>
+ <h4>Aktivitas Favorit</h4>
+ <ul type="square">
+ <li>Tech Enthusiast</li>
+ <li>Futsal</li>
+ </ul>
+</section>
